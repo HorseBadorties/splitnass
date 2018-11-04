166aa635831105494c029f90f33550cf95628adb
HorseBadorties <HorseBadorties@whereever.com>
2018-11-04 21:53:47 +0100

    readLatestSpieltagFromDB

--- src/server/server.ts
+++ src/server/server.ts
@@ -18,73 +18,85 @@ export class SplitnassServer {
     private websocket: socketIo.Server;
     private db: Db;
     private dbCollection: Collection;
-    private aktSpieltag: Spieltag;
+    private aktSpieltag: string;
 
     constructor() {
-      this.app = express();
-      // // Parse application/json
-      // this.app.use(bodyParser.json());
-      // // Parse application/vnd.api+json as json
-      // this.app.use(bodyParser.json({ type: "application/vnd.api+json" }));
-      // // Parse application/x-www-form-urlencoded
-      // this.app.use(bodyParser.urlencoded({ extended: true }));
+        this.app = express();
+        // // Parse application/json
+        // this.app.use(bodyParser.json());
+        // // Parse application/vnd.api+json as json
+        // this.app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+        // // Parse application/x-www-form-urlencoded
+        // this.app.use(bodyParser.urlencoded({ extended: true }));
 
-      // Run the app by serving the static files
-      // in the dist directory
-      this.app.use(express.static(__dirname + "/../../dist/splitnass"));
+        // Run the app by serving the static files
+        // in the dist directory
+        this.app.use(express.static(__dirname + "/../../dist/splitnass"));
 
-      // For all GET requests, send back index.html
-      // so that PathLocationStrategy can be used
-      this.app.get("/*", function(req, res) {
-        res.sendFile(path.join(__dirname + "/../../dist/splitnass/index.html"));
-      });
-      const server = createServer(this.app);
-      this.websocket = socketIo(server);
-      this.websocket.origins("*:*");
-      this.websocket.on("connect", socket => {
-          console.log(`Client ${socket.client.id} connected`);
-          socket.on("disconnect", () => {
-              console.log(`Client ${socket.client.id} disconnected`);
-          });
-          socket.on("spieltag", data => {
-            console.log(`sending updated spieltag`);
-            this.aktSpieltag = data;
-            this.websocket.compress(true).emit("spieltag", data);
-            if (this.dbCollection) {
-                const spieltagObject = JSON.parse(data);
-                this.dbCollection.replaceOne({"key": spieltagObject.key},
-                    spieltagObject, {"upsert": true}, (_err: MongoError, result: any) => {
-                        if (_err) {
-                            console.error(_err);
-                        } else {
-                            console.log(`updated ${spieltagObject.key} on db`);
-                        }
-                    });
-                }
+        // For all GET requests, send back index.html
+        // so that PathLocationStrategy can be used
+        this.app.get("/*", function (req, res) {
+            res.sendFile(path.join(__dirname + "/../../dist/splitnass/index.html"));
         });
-        socket.on("lastSpieltag", _ => {
-            if (this.aktSpieltag) {
-                console.log(`sending last spieltag`);
-                socket.compress(true).emit("lastSpieltag", this.aktSpieltag);
-                // setTimeout(() => socket.compress(true).emit("lastSpieltag", this.aktSpieltag), 3000);
+        const server = createServer(this.app);
+        this.websocket = socketIo(server);
+        this.websocket.origins("*:*");
+        this.websocket.on("connect", socket => {
+            console.log(`Client ${socket.client.id} connected`);
+            socket.on("disconnect", () => {
+                console.log(`Client ${socket.client.id} disconnected`);
+            });
+            socket.on("spieltag", data => {
+                console.log(`sending updated spieltag`);
+                this.aktSpieltag = data;
+                this.websocket.compress(true).emit("spieltag", data);
+                if (this.dbCollection) {
+                    const spieltagObject = JSON.parse(data);
+                    this.dbCollection.replaceOne({ "key": spieltagObject.key },
+                        spieltagObject, { "upsert": true }, (_err: MongoError, result: any) => {
+                            if (_err) {
+                                console.error(_err);
+                            } else {
+                                console.log(`updated ${spieltagObject.key} on db`);
+                            }
+                        });
+                }
+            });
+            socket.on("lastSpieltag", _ => {
+                if (this.aktSpieltag) {
+                    console.log(`sending last spieltag`);
+                    socket.compress(true).emit("lastSpieltag", this.aktSpieltag);
+                    // setTimeout(() => socket.compress(true).emit("lastSpieltag", this.aktSpieltag), 3000);
+                }
+            });
+        });
+
+        // MongoDB
+        MongoClient.connect(SplitnassServer.mongoUrl, { useNewUrlParser: true },
+            (_err: MongoError, _db: Db) => {
+                console.log(`successfully connected to ${SplitnassServer.mongoUrl}`);
+                this.db = _db.db("splitnass");
+                this.db.createCollection("spieltag", (_err2: MongoError, _coll: Collection) => {
+                    if (!_err2) {
+                        this.dbCollection = _coll;
+                        this.readLatestSpieltagFromDB();
+                    }
+                });
+            });
+
+        // start server
+        server.listen(this.port, () => {
+            console.log(`Splitnass server running on port ${this.port}`);
+        });
+    }
+
+    private readLatestSpieltagFromDB() {
+        this.dbCollection.find({}).sort({ key: -1 }).limit(1).next((_err: MongoError, result: any) => {
+            if (!_err) {
+                this.aktSpieltag = JSON.stringify(result);
+                console.log(`Read latest Spieltag ${result.key} from db`);
             }
         });
-      });
-
-      // MongoDB
-      MongoClient.connect(SplitnassServer.mongoUrl, { useNewUrlParser: true },
-        (_err: MongoError, _db: Db) => {
-            console.log(`successfully connected to ${SplitnassServer.mongoUrl}`);
-            this.db = _db.db("splitnass");
-            this.db.createCollection("test", (_err2: MongoError, _coll: Collection) => {
-                this.dbCollection = _coll;
-        });
-      });
-
-      // start server
-      server.listen(this.port, () => {
-        console.log(`Splitnass server running on port ${this.port}`);
-      });
     }
 
     public getApp(): express.Application {
