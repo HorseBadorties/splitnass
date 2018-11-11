14edfa8271386979dbdd50dd8e3d98b93f5dbd60
HorseBadorties <HorseBadorties@whereever.com>
2018-11-11 11:52:41 +0100

    fiddling with animation

--- src/client/app/components/animations.ts
+++ src/client/app/components/animations.ts
@@ -6,30 +6,15 @@ import {
   // Routable animations
   export const fadeInAnimation =
     trigger('routeAnimations', [
-      transition('Rundenliste <=> Runde', [
-        style({ position: 'relative' }),
-        query(':enter, :leave', [
-          style({
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            height: '100%',
-            width: '100%'
-          })
-        ]),
-        // query(':enter', [
-        //   // style({ top: '-100%'})
-        // ]),
-        // query(':leave', animateChild()),
+      transition('Rundenliste <=> Runde', [        
         group([
           query(':leave', [
-            animate(300, style({opacity: 0}))
+            animate(500, style({opacity: 0}))
           ]),
           query(':enter', [
-            animate(300, style({opacity: 1}))
+            animate(500, style({opacity: 1}))
           ])
-        ]),
-        // query(':enter', animateChild()),
+        ])
       ])
     ]);
 
