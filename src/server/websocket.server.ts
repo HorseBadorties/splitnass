6f28d17e17f240a752cfa3e60eebbb40b38001a1
HorseBadorties <HorseBadorties@whereever.com>
2018-11-24 21:31:51 +0100

    prevent concurrent updates

--- src/server/websocket.server.ts
+++ src/server/websocket.server.ts
@@ -5,7 +5,8 @@ import { SplitnassServer } from "./server";
 
 export class WebsocketServer {
   private io: socketIo.Server;
-  private rooms = new Map<string, string>();
+  private rooms = new Map<string, string>(); // client.id to Spieltag.beginn
+  private admins = new Map<string, string>(); // Spieltag.beginn to client.id
 
   constructor(private splitnassServer: SplitnassServer) {
     this.io = socketIo(splitnassServer.getHttpServer());
@@ -17,13 +18,21 @@ export class WebsocketServer {
   private onConnect(socket: socketIo.Socket) {
     console.log(`Client ${socket.client.id} connected`);
     socket.on("disconnect", () => {
+      this.rooms.delete(socket.client.id);
+      this.removeAsAdmin(socket);
       console.log(`Client ${socket.client.id} disconnected`);
     });
     socket.on("spieltagUpdated", (spieltagJSON: string, beginnJSON: string) => {
-      this.addClientToRoom(socket, beginnJSON);
-      console.log(`spieltag ${beginnJSON} got updated`);
-      this.io.compress(true).to(beginnJSON).emit("spieltagUpdated", spieltagJSON);
-      this.splitnassServer.spieltagUpdate(spieltagJSON);
+      if (this.makeAdminFor(socket, beginnJSON)) {
+        this.addClientToRoom(socket, beginnJSON);
+        console.log(`spieltag ${beginnJSON} got updated`);
+        this.io.compress(true).to(beginnJSON).emit("spieltagUpdated", spieltagJSON);
+        this.splitnassServer.spieltagUpdate(spieltagJSON);
+      } else {
+        // client was trying to update the Spieltag but is not an admin
+        console.log(`spieltag ${beginnJSON} not updated because ${socket.client.id} is not the admin`);
+        socket.emit("notAnAdmin");
+      }
     });
     socket.on("listSpieltage", () => {
       this.splitnassServer.listSpieltage().then(list => {
@@ -41,8 +50,28 @@ export class WebsocketServer {
     });
   }
 
+  private makeAdminFor(clientSocket: socketIo.Socket, room: string) {
+    const currentAdmin = this.admins.get(room);
+    if (currentAdmin === clientSocket.client.id) { // client is admin already
+      return true;
+    } else if (!currentAdmin) { // make him admin
+      this.admins.set(room, clientSocket.client.id);
+      console.log(`client ${clientSocket.client.id} now admin of ${room}`);
+      return true;
+    } else { // there is already another admin
+      return false;
+    }
+  }
+
+  private removeAsAdmin(clientSocket: socketIo.Socket) {
+    this.admins.forEach((v, k, m) => {
+      if (v === clientSocket.client.id) m.delete(k);
+    });
+  }
+
   private addClientToRoom(clientSocket: socketIo.Socket, room: string) {
     if (this.rooms.get(clientSocket.client.id) !== room) {
+      this.removeAsAdmin(clientSocket);
       clientSocket.leave(this.rooms.get(clientSocket.client.id));
       clientSocket.join(room);
       this.rooms.set(clientSocket.client.id, room);
